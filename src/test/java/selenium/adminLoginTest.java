92edf91c4fefebb2613e02fd5a452f17d61f1d67
ivanrightman <vivasvse@gmail.com>
2020-10-13 10:52:22 +0500

    Задания 4-6

--- src/test/java/selenium/adminLoginTest.java
+++ src/test/java/selenium/adminLoginTest.java
@@ -5,16 +5,38 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 
 public class adminLoginTest {
     private WebDriver driver;
+    private FirefoxOptions options = new FirefoxOptions();
 
     @Before
     public void start() {
+        /*
+        //Задание 4. Научитесь запускать разные браузеры
         driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+        driver = new InternetExplorerDriver();
+         */
+
+        /*
+        //Задание 5. Научитесь запускать Firefox ESR "по старой схеме"
+        options
+                .setLegacy(false)
+                .setBinary("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe");
+         */
+
+        //Задание 6. Научитесь запускать Firefox Nightly
+        options
+                .setBinary("C:\\Program Files\\Firefox Nightly\\firefox.exe");
+        driver = new FirefoxDriver(options);
     }
+
 
     @Test
     public void myFirstTest() {
