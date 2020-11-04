aaa48dd3ada51f0aef33315b48a6e9644522b8e7
ivanrightman <vivasvse@gmail.com>
2020-11-04 19:07:25 +0500

    Задание 9. Проверить сортировку стран и геозон в админке

--- src/test/java/selenium/AdminTests.java
+++ src/test/java/selenium/AdminTests.java
@@ -3,6 +3,9 @@ package selenium;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,7 +19,7 @@ public class AdminTests extends TestBase {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        boolean elem = isElementPresent(driver, By.linkText("Appearence"));
+        boolean elem = isElementPresent(driver, By.cssSelector("[title *= Database]"));
         assertThat(elem, is(true));
     }
 
@@ -37,6 +40,77 @@ public class AdminTests extends TestBase {
                     assertThat(isElementPresent(driver, By.tagName("h1")), is(true));
                 }
             }
+        }
+    }
+
+    @Test
+    public void ifCountriesSortedInAlphabetOrder() {
+        if (!areElementsPresent(driver, By.cssSelector("[title *= Database]"))) {
+            adminLoginTest();
+        }
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        ArrayList<String> countryNames = new ArrayList();
+        ArrayList<String> countryNamesSort = new ArrayList();
+        List<WebElement> rows = getElsByTwoStep(By.id("content"), By.className("row"));
+        for (int i = 0; i < rows.size(); i++) {
+            List<WebElement> currentRows = getElsByTwoStep(By.id("content"), By.className("row"));
+            String countryName = currentRows.get(i).findElement(By.xpath("./td[5]")).getText();
+            countryNames.add(countryName);
+            int n = Integer.parseInt(currentRows.get(i).findElement(By.xpath("./td[6]")).getText());
+            if (n > 0) {
+                currentRows.get(i).findElement(By.xpath("./td[7]")).click();
+                ArrayList<String> zoneNames = new ArrayList();
+                ArrayList<String> zoneNamesSort = new ArrayList();
+                List<WebElement> rowsZone = getElsByTwoStep(By.id("table-zones"), By.cssSelector("tr:not(.header)"));
+                for (WebElement rowz : rowsZone) {
+                    String zoneName = rowz.findElement(By.xpath("./td[3]")).getText();
+                    if (!zoneName.equals("")) {
+                        zoneNames.add(zoneName);
+                    }
+                }
+                for (String namez : zoneNames) {
+                    zoneNamesSort.add(namez);
+                }
+                Collections.sort(zoneNamesSort);
+                assertThat(zoneNames, is(zoneNamesSort));
+                driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+            }
+        }
+        for (String name : countryNames) {
+            countryNamesSort.add(name);
+        }
+        Collections.sort(countryNamesSort);
+        assertThat(countryNames, is(countryNamesSort));
+    }
+
+    @Test
+    public void ifGeoZonesSortedInAlphabetOrder() {
+        if (!areElementsPresent(driver, By.cssSelector("[title *= Database]"))) {
+            adminLoginTest();
+        }
+        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+        List<WebElement> rows = getElsByTwoStep(By.id("content"), By.className("row"));
+        for (int i = 0; i < rows.size(); i++) {
+            List<WebElement> currentRows = getElsByTwoStep(By.id("content"), By.className("row"));
+            currentRows.get(i).findElement(By.xpath("./td[5]")).click();
+            ArrayList<String> zoneNames = new ArrayList();
+            ArrayList<String> zoneNamesSort = new ArrayList();
+            List<WebElement> rowsZone = getElsByTwoStep(By.id("table-zones"), By.cssSelector("tr:not(.header)"));
+            for (WebElement rowz : rowsZone) {
+                List<WebElement> elems = rowz.findElements(By.xpath("./td[3]/select/option"));
+                for (WebElement el : elems) {
+                    boolean n = Boolean.parseBoolean(el.getAttribute("selected"));
+                    if (n) {
+                        zoneNames.add(el.getAttribute("textContent"));
+                    }
+                }
+            }
+            for (String name : zoneNames) {
+                zoneNamesSort.add(name);
+            }
+            Collections.sort(zoneNamesSort);
+            assertThat(zoneNames, is(zoneNamesSort));
+            driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
         }
     }
 }
